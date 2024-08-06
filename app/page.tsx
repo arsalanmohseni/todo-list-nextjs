85257be4c53d3dc0e77ce3fef6adcf3982fbab11
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-08-06 19:14:31 +0330

    Deleted background component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import Background from "./Components/Background";
+import React from 'react';
 
 const page = () => {
-    return (
-        <div>
-            <h1 className="text-3xl font-bold underline">Todolister</h1>
-        </div>
-    );
+	return (
+		<div>
+			<h1 className="text-3xl font-bold underline">Todolister</h1>
+		</div>
+	);
 };
 
 export default page;
